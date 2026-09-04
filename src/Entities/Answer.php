4a98e94952153befd59a8d53eef80d387d97c8d0
morgan1000 <morgan.gardes.19@gmail.com>
2026-09-04 10:52:07 +0200

    màj answer.php

--- src/Entities/Answer.php
+++ src/Entities/Answer.php
@@ -2,16 +2,23 @@
 class Answer
 {
     private string $answer;
+    private bool $isCorrect;
 
        public function __construct(
         string $answer
     )
     {
        $this->answer = $answer;
+       $this->isCorrect = $isCorrect;
     }
 
     public function getAnswer(): string
      {
         return $this->answer;
+    }
+
+    public function isCorrect(): bool
+    {
+        return $this->isCorrect;
     }
 }
