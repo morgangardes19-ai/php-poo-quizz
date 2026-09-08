d3f3b7e0833c4257a1701df21eac7cb918ea65ca
morgan1000 <morgan.gardes.19@gmail.com>
2026-09-08 14:15:07 +0200

    affichage du score : OK

--- public/score.php
+++ public/score.php
@@ -1,3 +1,8 @@
+<?php
+require_once "../utils/autoloader.php";
+session_start(); 
+?> 
+
  <h1>Quiz terminé !</h1>
- <p>Ton score : <?= $_SESSION["score"] ?> points, pour <?= $_SESSION["score"] ?>/<?= $_SESSION["questions"] ?> justes. </p>
+ <p>Ton score : <?= $_SESSION["score"] ?> points, pour <?= $_SESSION["score"] ?>/<?= count($_SESSION["questions"]) ?> réponses justes. </p>
  <a href="index.php">Retour à la liste des quiz</a>
